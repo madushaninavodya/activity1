8587072aeabf7a4aa4ea66b02d7cbf83b1532582
madushaninavodya <madushaninavodya6@gmail.com>
2018-02-07 00:59:44 -0800

    Add  New Colors

--- index.php
+++ index.php
@@ -10,21 +10,37 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div class="container">
-<div class="row" id="nav">
-        <nav class="navbar navbar-inverse" >
+    
+<div class="row" id="header">
+      <div class="col-md-12">
+          <div class="row" >
+          <div class="col-md-6">
+             
+</div>
+          <div class="col-md-6">
+    <h1>Beauty of Nature</h1>
+</div>
+
+</div>
+</div>
+</div>
+            <div class="container">
+            <div class="row">
+     <div class="col-md-12">
+        <nav class="navbar navbar-inverse" id="" >
             <div class="container-fluid">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="#"> Rose</a>
                 </div>
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">About Us</a></li>
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About Us</a></li>
                     <li><a href="#">Contact Us</a></li>
                     <li><a href="#">Gallery</a></li>
                 </ul>
             </div>
         </nav>
+   </div>
    </div>
             
             
