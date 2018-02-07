d159888e418e46a1c221acbea5749f6074ab4f65
madushaninavodya <madushaninavodya6@gmail.com>
2018-02-07 00:11:57 -0800

    Add About Us

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Activity1</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link  rel="stylesheet" href="newcss.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row" id="nav">
+                <nav class="navbar navbar-inverse" >
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#"> Rose</a>
+                        </div>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li ><a href="#">Home</a></li>
+                            <li class="active"><a href="about.php">About Us</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Gallery</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </body>
+</html>
