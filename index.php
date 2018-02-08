9fc6fe0b3aa6b88412868b74518c64fac6a8007f
madushaninavodya <madushaninavodya6@gmail.com>
2018-02-08 02:55:08 -0800

    Complete Index Page

--- index.php
+++ index.php
@@ -4,78 +4,114 @@
         <title>Activity1</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link  rel="stylesheet" href="newcss.css">
+
+
+        <link href="newcss.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" type="text/css"/>
+        <link href="owlcarousel/assets/owl.theme.default.min.css" rel="stylesheet" type="text/css"/>
+        <link href="owlcarousel/assets/owl.theme.green.min.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Playball" rel="stylesheet"> 
+
+        <style>
+
+            .image1{
+                height: 80px;
+                width: 80px;
+                margin-right: 420px;
+                padding-top: 5px;
+                padding-bottom: 5px;
+            }    #image2{
+                height: 340px;
+                width: 480px;
+                border-radius: 6px;
+            }
+            #image3{
+                border-radius: 6px;
+                height: 260px;
+                width: 280px;
+            }
+
+            #header{
+                background:#803e53;
+                text-align: center;
+                color:blue;
+                font-family: 'Playball', cursive;
+
+
+            }
+        </style>
+
     </head>
     <body>
-    
-<div class="row" id="header">
-      <div class="col-md-12">
-          <div class="row" >
-          <div class="col-md-6">
-             
-</div>
-          <div class="col-md-6">
-    <h1>Beauty of Nature</h1>
-</div>
 
-</div>
-</div>
-</div>
-            <div class="container">
-            <div class="row">
-     <div class="col-md-12">
-        <nav class="navbar navbar-inverse" id="" >
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#"> Rose</a>
-                </div>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="index.php">Home</a></li>
-                    <li><a href="about.php">About Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Gallery</a></li>
-                </ul>
+        <div class="row" id="header">
+            <div class="col-md-12">
+
+                <h1>Beauty of Nature</h1>
             </div>
-        </nav>
-   </div>
-   </div>
-            
-            
-         <div class="row">
-                <div class="col-xs-12">
-                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                        </ol>
 
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <img src="images/1.jpeg" alt=""/>
-                                <div class="carousel-caption">
-                                    ...
-                                </div>
+        </div>
+
+        <div class="example3">
+            <nav class="navbar navbar-inverse navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+
+                    </div>
+                    <div id="navbar3" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Gallery</a></li>
+                            <li><a href="#">News</a></li>
+
+                        </ul>
+                    </div>
+                    <!--/.nav-collapse -->
+                </div>
+                <!--/.container-fluid -->
+            </nav>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox" >
+                        <div class="item active" >
+                            <img src="images/1.jpeg" alt=""/>
+                            <div class="carousel-caption">
+                                First Image
                             </div>
-                            <div class="item">
-                                <img src="images/2.jpeg" alt=""/>
-                                <div class="carousel-caption">
-                                    ...
-                                </div>
-                            </div>
-                            <div class="item">
-                                <img src="images/3.jpg" alt=""/>
-                                <div class="carousel-caption">
-                                    ...
-                                </div>
-                            </div>
-                         
                         </div>
+                        <div class="item" >
+                            <img src="images/2.jpeg" alt=""/>
+                            <div class="carousel-caption">
+                                Second Image
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/3.jpg" alt=""/>
+                            <div class="carousel-caption">
+                                Third Image
+                            </div>
+                        </div>
+
+
 
                         <!-- Controls -->
                         <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
@@ -87,35 +123,172 @@
                             <span class="sr-only">Next</span>
                         </a>
                     </div>
+
+
                 </div>
             </div>
-            <br>
-            <br>
-            <br>
-            <br>
-
-            <div clas="row">
-                <div class="col-sm-4 col-sm-push-8">
-                    <img src="images/person.jpg" alt="" class="img-responsive img-rounded"/>
-                    <h2 class="text-center">Jimy</h2>
-                </div>
-                <div class="col-sm-8 col-sm-pull-4">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-               
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-            </div>
-
-
-
-
-        
-            
         </div>
+        <br/>
+        <br/>
+        <br/>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="container">
+
+        <div clas="row">
+         
+            <div class="col-sm-4 col-sm-push-8">
+                <img src="images/person.jpg" alt="" class="img-responsive img-rounded"/>
+                <h2 class="text-center">Jimy</h2>
+            </div>
+            <div class="col-sm-8 col-sm-pull-4">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </div>
+   </div> 
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+        <div class="owl-carousel  owl-theme" >
+
+            <div>  <img src="images/im2.jpg" alt=""/></div>
+            <div>  <img src="images/im3.jpg" alt=""/></div>
+            <div>  <img src="images/img1.jpg" alt=""/></div>
+            <div>  <img src="images/img2.jpg" alt=""/></div>
+            <div>  <img src="images/2.jpeg" alt=""/></div>
+            <div>  <img src="images/3.jpg" alt=""/></div>
+       
+            
+        </div> 
+     
+    
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+
+
+
+
+        <!--FOOTER 2-->
+        <footer class="footer2">
+            <div class="footer">
+                <div class="container">
+                    <div class="row">
+                        <nav class="col-md-3 col-sm-6 col-xs-12">
+                            <h3>Browse <span>Categories</span></h3>
+                            <ul>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                  
+                            </ul>
+                        </nav>
+                        <nav class="col-md-3 col-sm-6 col-xs-12">
+                            <h3>How <span>to Buy</span></h3>
+                            <ul>
+                                 <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                  
+                            </ul>
+                        </nav>
+                        <nav class="col-md-3 col-sm-6 col-xs-12">
+                            <h3>Help <span>& Support</span></h3>
+                            <ul>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                <li>  <a href="#">Lorem Ipsum</a> </li>
+                                  
+                               
+                                
+                            </ul>
+                        </nav>
+
+                        <nav class="col-md-3 col-sm-6 col-xs-12">
+                            <h3>Lorem <span>Ipsum</span></h3>
+                            <div class="input-group input-group-md">
+                                <input type="text" class="form-control" placeholder="Email Address">
+                                <span class="input-group-addon">Subscribe</span>
+                            </div>
+                            <ul class="social">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                            </ul>
+                        </nav>
+
+                    </div> <!--/.row--> 
+                </div> <!--/.container--> 
+            </div> <!--/.footer-->
+
+
+        </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="owlcarousel/owl.carousel.min.js" type="text/javascript"></script>
+        <script>
+            $(document).ready(function () {
+                $('.owl-carousel').owlCarousel({
+
+                    loop: true,
+                    margin: 10,
+                    nav: true,
+                    dots: true,
+
+                    responsive: {
+                        0: {
+                            items: 1
+                        },
+                        600: {
+                            items: 2
+                        },
+                        1000: {
+                            items: 3
+                        },
+                      
+                        1024: {
+                            items: 3
+                        },
+                    },
+
+                    autoplay: true,
+                    autoplayTimeout: 2500,
+                    autoplayHoverPause: true,
+                    animateOut: true,
+
+                });
+            });
+        </script>
+
 
     </body>
 </html>
